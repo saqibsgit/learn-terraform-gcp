82e2e57290477694ecae01fd29f4a8e6850d57f2
saqib.ali <saqi.ali@icloud.com>
2025-03-02 16:12:57 +0100

    changing structure of project

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.23.0"
-    }
-  }
-}
-
-provider "google" {
-  # Configuration options
-  project = "terraform-practice-452511"
-  region  = "europe-west3"
-  zone    = "europe-west3-a"
-}
-
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
