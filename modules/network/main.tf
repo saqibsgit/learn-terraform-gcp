7b043b98aee0baa609b7acda8e4ac4ef11f58603
saqib.ali <saqi.ali@icloud.com>
2025-03-04 12:32:45 +0100

    adding subnet in network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,3 +2,17 @@ resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   auto_create_subnetworks = var.auto_create_subnetworks
 }
+
+resource "google_compute_subnetwork" "public_subnet" {
+  name          = "${var.vpc_name}-public-subnet"
+  ip_cidr_range = var.public_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+  name          = "${var.vpc_name}-private_subnet"
+  ip_cidr_range = var.private_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -9,3 +9,22 @@ variable "auto_create_subnetworks" {
   description = "Whether to create subnetworks"
   default     = true
 }
+
+variable "region" {
+  type        = string
+  description = "The region where resources will be created"
+  default     = "europe-west3"
+
+}
+
+variable "public_subnet_cidr" {
+  type        = string
+  description = "The CIDR block of the public subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type        = string
+  description = "The CIDR block of the private subnet"
+  default     = "10.0.2.0/24"
+}
