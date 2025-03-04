0eefc829960af337db91515e40dae0bb401668b9
saqib.ali <saqi.ali@icloud.com>
2025-03-04 17:30:42 +0100

    added code for secret manager module

---
+++ modules/secrets/main.tf
@@ -0,0 +1,22 @@
+resource "google_secret_manager_secret" "secret" {
+  name    = var.secret_name
+  project = var.project_id
+
+  replication {
+    automatic = true #replicate secret accross different regions 
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret_version" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = var.secret_value
+}
+
+# only privildeged roles can access the secret, in this case service account
+resource "google_secret_manager_secret_iam_member" "secret_access" {
+  secret_id = google_secret_manager_secret.secret.id
+  role      = "roles/secretmanager.secretAccessor"
+
+  member = "serviceAccount:${var.service_account_email}"
+}
+
