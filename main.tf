3a4018b85e3c1cc6bc73f69da105c507cce0d117
saqib.ali <saqi.ali@icloud.com>
2025-03-04 10:53:47 +0100

    add local backend

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
 
+# Adding backend to store the state file
+terraform {
+    backend "local"{
+        path = "./terraform.tfstate"
+    }
+}
+resource "google_storage_bucket" "remote-state-bucket"{
+    name = "terraform-practice-452511-terraform-state"
+    location = "EU"
+     uniform_bucket_level_access = true
+}
